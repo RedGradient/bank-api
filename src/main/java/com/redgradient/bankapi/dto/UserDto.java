72a710e66c03653ea3829da85164936c171db1d3
redgradient <neutral1shade@yahoo.com>
2024-03-02 23:06:49 +0400

    checkpoint

--- src/main/java/com/redgradient/bankapi/dto/UserDto.java
+++ src/main/java/com/redgradient/bankapi/dto/UserDto.java
@@ -1,5 +1,9 @@
 package com.redgradient.bankapi.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,10 +12,23 @@ import java.util.Date;
 @Getter
 @AllArgsConstructor
 public class UserDto {
+    @NotEmpty(message = "First name cannot be empty")
+    @Size(max = 50, message = "First name cannot exceed 50 characters")
     private String firstName;
+
+    @NotEmpty(message = "Last name cannot be empty")
+    @Size(max = 50, message = "Last name cannot exceed 50 characters")
     private String lastName;
+
+    @Size(max = 50, message = "Middle name cannot exceed 50 characters")
     private String middleName;
+
+    @NotEmpty(message = "Email cannot be empty")
+    @Email(message = "Invalid email format")
     private String email;
+
     private String phoneNumber;
+
+    @Past(message = "Date of birth must be in the past")
     private Date dateOfBirth;
 }
